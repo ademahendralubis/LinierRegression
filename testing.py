138ebea35550c1f0a6603917a62b0009f1528519
DESKTOP-MG7KIFU\codingan <ademahendralubis@gmail.com>
2021-07-23 16:37:32 +0700

    add testing.py

---
+++ testing.py
@@ -0,0 +1,31 @@
+from LinearRegression import LinearRegression
+import numpy as np
+
+if __name__ == "__main__":
+    # Imports
+    import matplotlib.pyplot as plt
+    from sklearn.model_selection import train_test_split
+    from sklearn import datasets
+    from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+    X, y = datasets.make_regression(
+        n_samples=100, n_features=1, noise=20, random_state=4
+    )
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
+
+    regressor = LinearRegression(learning_rate=0.01, n_iters=1000)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
+
+    mse = mean_squared_error(y_test, predictions)
+    mae = mean_absolute_error(y_test, predictions)
+    print("sklearn MSE:", mse)
+    
+    print("mse", regressor.mse(y_test, predictions))
+    
+    print("sklearn MAE:", mae)
+    
+    print("mae", regressor.mae(y_test, predictions))
